58513153fa370be781d5b32377d11a8743f4eed7
Fergal Cotter <fbcotter90@gmail.com>
2017-11-28 21:55:59 +0000

    Changed version read function

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 def read(fname):
     enc = 'utf-8'
     return open(os.path.join(os.path.dirname(__file__), fname), 'r',
-                encoding=enc).read()
+                encoding=enc).readlines()
 
 
 # Read metadata from version file
